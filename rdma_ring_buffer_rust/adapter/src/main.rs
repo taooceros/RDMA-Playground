ff9f218e54a22c8ecea3110864eac892ff1aa44d
Hongtao <hongtao_zhang@outlook.com>
2024-04-19 19:47:00 +0000

    update

--- rdma_ring_buffer_rust/adapter/src/main.rs
+++ rdma_ring_buffer_rust/adapter/src/main.rs
@@ -105,6 +105,8 @@ pub fn main() {
             unsafe {
                 let mut buffer = ring_buffer.reserve_write(message_size);
 
+                assert_eq!(buffer.len(), message_size);
+
                 ib_resource
                     .post_srq_recv(2, &mut mr, Out::<'_, [u8]>::from(buffer.deref_mut()))
                     .expect("Failed to post recv");
