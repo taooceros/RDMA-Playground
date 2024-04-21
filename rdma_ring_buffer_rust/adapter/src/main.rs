3f780b2967e5bad362854d29810e54d001146ed9
Hongtao <hongtao_zhang@outlook.com>
2024-04-21 17:20:00 +0000

    4096

--- rdma_ring_buffer_rust/adapter/src/main.rs
+++ rdma_ring_buffer_rust/adapter/src/main.rs
@@ -177,25 +177,25 @@ pub fn main() {
 
                 unsafe {
                     ib_resource
-                        .post_send(2, &mut mr, reader.deref(), false)
+                        .post_send(2, &mut mr, reader.deref(), true)
                         .expect("Failed to post send");
                 }
 
-                // 'outer: loop {
-                //     for wc in ib_resource.poll_cq() {
-                //         if wc.status != rdma_sys::ibv_wc_status::IBV_WC_SUCCESS {
-                //             panic!(
-                //                 "wc status {}, last error {}",
-                //                 wc.status,
-                //                 std::io::Error::last_os_error()
-                //             );
-                //         }
+                'outer: loop {
+                    for wc in ib_resource.poll_cq() {
+                        if wc.status != rdma_sys::ibv_wc_status::IBV_WC_SUCCESS {
+                            panic!(
+                                "wc status {}, last error {}",
+                                wc.status,
+                                std::io::Error::last_os_error()
+                            );
+                        }
 
-                //         if wc.opcode == rdma_sys::ibv_wc_opcode::IBV_WC_SEND {
-                //             break 'outer;
-                //         }
-                //     }
-                // }
+                        if wc.opcode == rdma_sys::ibv_wc_opcode::IBV_WC_SEND {
+                            break 'outer;
+                        }
+                    }
+                }
             }
         },
     }
